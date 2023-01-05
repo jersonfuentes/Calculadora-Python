bd314c65f7430e29bf74bbf721fb649848333e17
Jerson Fuentes <47575457+jersonfuentes@users.noreply.github.com>
2023-01-05 04:15:00 +0000

    Código inicial de la calculadora.

---
+++ main.py
@@ -0,0 +1,12 @@
+#Calculadora con la operaciones de matemáticas básicas
+valor1 = 7
+valor2 = 4
+
+resultadoS= valor1 + valor2
+resultadoR = valor1 - valor2 
+resultadoM = valor1 * valor2 
+resultadoD = valor1 / valor2 
+#Método por formato
+print(f'La suma es: {resultadoS}, la resta es: {resultadoR}, la multiplicacion es: {resultadoM}, la Division es: {resultadoD}')
+#Método Tradicional
+print("La suma es:" ,resultadoS, "la resta es: " ,resultadoR," la multiplicacion es: ",resultadoM, "la Division es: ",resultadoD)
